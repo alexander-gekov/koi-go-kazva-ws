b5c49bfdb5763a0680e143d01cf4e4d9300ecf62
Alexander Gekov <alexander.gekov00@gmail.com>
2023-03-15 13:05:03 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {  
+    cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],}
+});
+
+const rooms = {};
+
+io.on('connection', (socket) => {
+    socket.on('message', (data) => {
+        switch (data['message']) {
+            case 'create':
+                socket.join(data['room'])
+                rooms[data['room']] = data['data']
+                rooms[data['room']].owner = socket.id
+                break;
+            case 'join':
+                socket.join(data['room'])
+                const roomId = data['room']
+                if(rooms[roomId]) {
+                    socket.emit('message', {message: 'loadGameData', data: rooms[roomId]})
+                }
+                break
+            case 'leave':
+                socket.leave(data['room'])
+                if(socket.rooms.size == 0) {
+                    delete rooms[data['room']]
+                }
+                break
+        }
+    })
+
+    socket.on('disconnect', () => {
+        console.log('user ' + socket.id + ' disconnected');
+      });
+})
+
+httpServer.listen(8080);
+
