b421e762e63b8f2e5941d98d15d9141956383608
Aleksandar Gekov <alexander.gekov00@gmail.com>
2023-03-24 01:24:35 +0100

    update socket server

--- index.js
+++ index.js
@@ -20,20 +20,23 @@ const io = new Server(httpServer, {
 
 const rooms = {};
 
-io.on('connection', (socket) => {
+o.on('connection', (socket) => {
     socket.on('message', (data) => {
         switch (data['message']) {
             case 'create':
                 socket.join(data['room'])
                 rooms[data['room']] = data['data']
-                rooms[data['room']].owner = socket.id
+                rooms[data['room']].owner = socket.id;
+                rooms[data['room']].players = [data['username']];
                 break;
             case 'join':
                 socket.join(data['room'])
                 const roomId = data['room']
                 if(rooms[roomId]) {
+                    rooms[roomId].players.push(data['username'])
                     socket.emit('message', {message: 'loadGameData', data: rooms[roomId]})
-                    socket.emit('message', {message: 'join', data: data['username']})
+                    socket.emit('message', {message: 'loadPlayers', data: [... new Set(rooms[roomId].players)]})
+                    socket.to(roomId).emit('message', {message: 'loadPlayers', data: [... new Set(rooms[roomId].players)]})
                 }
                 break
             case 'leave':
